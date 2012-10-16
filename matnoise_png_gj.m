91d8037c2780a172814a76e220b93581be794619
Ge Jin <jinwar@gmail.com>
2012-10-16 10:40:55 -0400

    first try on database access

--- matnoise_png_gj.m
+++ matnoise_png_gj.m
@@ -2,4 +2,25 @@
 %  to do the ambient noise measurement for PNG dataset.
 %  Author: Ge Jin, jinwar@gmail.com
 
+% Parameter setting
+%
+dbpath = 'cdpapuall';
+component = 'BHZ';
+
+% Initial the dataset
+
+db = dbopen(cdpath,'r');
+dbwf=dblookup_table(db,'wfdisc');
+  subsetcomp=sprintf('chan=~/%s/',component);
+dbwf=dbsubset(dbwf,subsetcomp);
+dbsi=dblookup_table(db,'site');
+
+%gather stn info
+slats=zeros(1,length(stations)); slons=zeros(1,length(stations));
+for ind=1:length(stations)
+      thisstn=char(stations(ind));
+      ksite=dbfind(dbsi,sprintf('sta=~/%s/',thisstn));
+      dbsi.record=ksite;
+      [slats(ind) slons(ind)]=dbgetv(dbsi,'lat','lon');
+end
 
