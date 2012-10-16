f3b786196e0cdb03cd1d903d39596b3b7e7b42f8
Ge Jin <jinwar@gmail.com>
2012-10-16 16:39:53 -0400

    first tested version for station gathering. Setup and start to be tested on the server

--- matnoise_png_stainfo.m
+++ matnoise_png_stainfo.m
@@ -33,16 +33,26 @@ array_bgtime = (datenum(2010,3,2) - datenum(1970,1,1))*24*3600;
 array_endtime = (datenum(2011,8,1) - datenum(1970,1,1))*24*3600;
 time_interval = 3600;
 timegrids = array_bgtime:time_interval:array_endtime;
+NT = length(timegrids)
 
 for ista = 1:length(stainfo)
+	disp(['Working on gathering station ', stainfo(ista).staname, ' information']);
 	stainfo(ista).datacover = zeros(length(timegrids)-1,1);
-	for itime = 1:length(timegrids)-1
+	oldprocess=0;
+	for itime = 1:NT-1
+		newprocess = floor(itime/NT*100);
+		if newprocess>oldprocess
+			oldprocess = newprocess;
+			disp(['Process: ', num2str(newprocess),'%, Data Coverage: '...
+					,num2str(sum(stainfo(ista).datacover)/NT*100),'%']);
+		end
 		% try to find the records that has overlap with this time interval
 		ts = timegrids(itime);
 		te = timegrids(itime+1);
 		substr1=sprintf('(sta =~/%s/) && ((wfdisc.time <= %d && wfdisc.endtime > %d ) || (wfdisc.time > %d && wfdisc.time <%d) || (wfdisc.endtime > %d && wfdisc.endtime <%d) )',stainfo(ista).staname,ts,te,ts,te,ts,te);
 		dbtr1=dbsubset(dbwf,substr1);
 		if (dbquery(dbtr1,'dbRECORD_COUNT'))==0
+%			disp(['Cannot find data for itime:',num2str(itime)]);
 			continue;
 		end
 		% request dataset pointer
@@ -51,12 +61,19 @@ for ista = 1:length(stainfo)
 		trsplice(trptr1,50);
 		% if the trace number is larger than 1, means the segments cannot be spliced.
 		if (dbnrecs(trptr1)~=1)
+			disp(['Failed to merge segments for itime:',num2str(itime)]);
+			trdestroy(trptr1);
 			continue;
 		end
 		% Check the start time and end time are correct
-		if dbgetv(trptr1,'time')~=ts || dbgetv(trptr1,'endtime')~=te
+		if abs(dbgetv(trptr1,'time')-ts)>1 || abs(dbgetv(trptr1,'endtime')-te)>1
+			disp(['Incorrect beginning time and end time for itime:',num2str(itime)]);
+			trdestroy(trptr1);
 			continue;
 		end
 		stainfo(ista).datacover(itime)=1;
+		trdestroy(trptr1);
 	end
 end
+
+save('stainfo_BHZ','stainfo')
